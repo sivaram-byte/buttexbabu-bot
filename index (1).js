403d6002c7d571722805c17e512fcb335a77d6fd
sivaram-byte <76906134+sivaram-byte@users.noreply.github.com>
2021-07-12 09:11:05 +0530

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,11 @@
+  
+const mineflayer = require('mineflayer')
+const { keep_alive } = require("./keep_alive");
+
+
+const bot = mineflayer.createBot({
+  host: 'scarnmrp.aternos.me',
+  username: 'Scars Bot'
+})
+
+
